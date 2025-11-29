4139b750b2bf40e308b6ff4fc16b1d9e5596e92a
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-11-29 19:32:52 +0530

    Modifying the database for scalability

--- src/config/float.ts
+++ src/config/float.ts
@@ -1,2 +1,9 @@
 import { Request, Response, NextFunction } from "express";
+
 import env from "../config/env";
+import {  } from "../config/float";
+
+export const floatConfig = {
+    precision: 2,
+    scale:  2,
+};
