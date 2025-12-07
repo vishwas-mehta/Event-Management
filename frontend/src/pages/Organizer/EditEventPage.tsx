cc71fa5940e00710cfd6cc38078cd683234593f2
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-07 18:12:00 +0530

    Edit Event Page

---
+++ frontend/src/pages/Organizer/EditEventPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Alert } from 'react-bootstrap';
+
+const EditEventPage: React.FC = () => {
+    return (
+        <Container className="py-4">
+            <Alert variant="info">
+                Edit Event page - Similar to Create Event page but pre-filled with existing data.
+                Coming soon!
+            </Alert>
+        </Container>
+    );
+};
+
+export default EditEventPage;
